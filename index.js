ed94c4d81455fb90a4a2532de084b3b40c641a74
Rômulo Oliveira Candido da Silva <romulocand@gmail.com>
2018-07-08 13:58:01 -0300

    dgf

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('index')
+// Catch all routes and redirect to the index file
+app.get('*', function (req, res) {
+    res.sendFile(__dirname + '/public/index.html')
 })
 
 app.listen(app.get('port'), function() {
