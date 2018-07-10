86d8d8efb5b2fbfe288bd7d6b4631ecac5bca579
yseadi <yaaseadi@gmail.com>
2018-07-10 10:29:45 -0300

    teste

---
+++ js/constant.js
@@ -0,0 +1,6 @@
+﻿var idEmpresa = '744AAD94-48BC-4C54-A531-0E41871B98C4';
+var ServiceBasePath = 'https://sirros.azurewebsites.net/v1/';
+var emailReverse = 'contato@reverseresiduos.com.br';
+var headers = { "Content-Type": "application/json", "idEmpresa": idEmpresa };
+var urlReferrer = window.location.origin;
+
